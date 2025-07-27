4098ce4dcdfb083dbe48ac9163d0979048ac1645
VeroniJrStudant <veroni_vieira-jun1@estudante.sesisenai.org.br>
2025-07-27 15:27:06 -0300

    Retirado do gitignore o config.js.

---
+++ config.js
@@ -0,0 +1,10 @@
+// Configurações do sistema
+const CONFIG = {
+  // Google OAuth2 Configuration
+  GOOGLE_CLIENT_ID: '738193075799-6ginm252beh55eq0vju667gsrf3jnu8e.apps.googleusercontent.com',
+  GOOGLE_REDIRECT_URI: 'https://veronijrstudant.github.io/despachantoria-cema/',
+  GOOGLE_SCOPE: 'https://www.googleapis.com/auth/spreadsheets'
+};
+
+// Exportar para uso global
+window.CONFIG = CONFIG;
