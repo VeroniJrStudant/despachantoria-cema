3de98c7064fdebdf2b992adda63d2e23e0bc5bdc
Veroni Júnior <veroni_vieira-jun1@estudante.sesisenai.org.br>
2025-08-01 16:42:09 -0300

    Update config.js
    Novo redirecionamento de domínio

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
 window.CONFIG = {
   GOOGLE_CLIENT_ID: '738193075799-6ginm252beh55eq0vju667gsrf3jnu8e.apps.googleusercontent.com',
   GOOGLE_CLIENT_ID_NAO_AUTORIZADO: '',
-  GOOGLE_REDIRECT_URI: 'https://veronijrstudant.github.io/despachantoria-cema/',
+  // GOOGLE_REDIRECT_URI: 'https://veronijrstudant.github.io/despachantoria-cema/',
+  GOOGLE_REDIRECT_URI: 'https://veroni.tec.br',
   GOOGLE_SCOPE: 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive'
 };
