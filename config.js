3e36afd4b72eb664535cc40abf2af840dca7097b
VeroniJrStudant <VeroniJrStudant@users.noreply.github.com>
2025-08-07 01:38:26 +0000

    deploy: 21e106651a1990d1f6cda131d80da6e028d72c50

---
+++ config.js
@@ -0,0 +1,7 @@
+// Arquivo gerado automaticamente pelo GitHub Actions
+window.CONFIG = {
+  GOOGLE_CLIENT_ID: '738193075799-6ginm252beh55eq0vju667gsrf3jnu8e.apps.googleusercontent.com',
+  GOOGLE_CLIENT_ID_NAO_AUTORIZADO: '',
+  GOOGLE_REDIRECT_URI: 'https://veronijrstudant.github.io/despachantoria-cema/',
+  GOOGLE_SCOPE: 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive',
+};
